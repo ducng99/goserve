d57053c2b5c94c4e51df8ae47721535293a36aa6
ducng99 <tom@tomng.dev>
2024-04-06 12:37:41 +1300

    refactor: Uses `templ` to render HTML templates

--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module r.tomng.dev/goserve
 go 1.22
 
 require (
+	github.com/a-h/templ v0.2.648
 	github.com/gookit/color v1.5.4
 	github.com/spf13/cobra v1.8.0
 )
@@ -10,6 +11,6 @@ require (
 require (
 	github.com/inconshreveable/mousetrap v1.1.0 // indirect
 	github.com/spf13/pflag v1.0.5 // indirect
-	github.com/xo/terminfo v0.0.0-20210125001918-ca9a967f8778 // indirect
-	golang.org/x/sys v0.12.0 // indirect
+	github.com/xo/terminfo v0.0.0-20220910002029-abceb7e1c41e // indirect
+	golang.org/x/sys v0.19.0 // indirect
 )
--- internal/tmpl/directory_view.go
+++ internal/tmpl/directory_view.go
@@ -1,41 +1,12 @@
 package tmpl
 
 import (
-	"html/template"
+	"context"
 	"net/http"
 
 	"r.tomng.dev/goserve/internal/files"
+	"r.tomng.dev/goserve/internal/tmpl/templates"
 )
-
-type DirectoryViewData struct {
-	Path  string
-	Entries []files.Entry
-}
-
-var DirectoryView = template.Must(template.New("directory_view").Parse(`
-<!DOCTYPE html>
-<html>
-<head lang="en">
-    <meta charset="UTF-8">
-	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Goserve Indexing</title>
-</head>
-
-<body>
-    <h1>Indexing - {{ .Path }}</h1>
-	<main>
-		<ul>
-			{{ range .Entries }}
-				<li><a href="/{{ .Path }}">{{ .Name }}</a></li>
-			{{ end }}
-		</ul>
-	</main>
-	<footer>
-		<i>Powered by <a href="https://r.tomng.dev/goserve">goserve</a></i>
-	</footer>
-</body>
-</html>
-`))
 
 func RenderDirectoryView(w http.ResponseWriter, rootDir, path string) {
 	entries, err := files.GetEntries(rootDir, path)
@@ -44,13 +15,10 @@ func RenderDirectoryView(w http.ResponseWriter, rootDir, path string) {
 		return
 	}
 
-	data := DirectoryViewData{
-		Path:  path,
-		Entries: entries,
-	}
-
 	w.Header().Set("Content-Type", "text/html")
-	if err := DirectoryView.Execute(w, data); err != nil {
+
+	templComp := templates.DirectoryView(path, entries)
+	if err := templComp.Render(context.TODO(), w); err != nil {
 		http.Error(w, "Internal server error", http.StatusInternalServerError)
 	}
 }
