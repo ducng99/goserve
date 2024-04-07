97e2ee2696da36beb1b8d36dec9d01a9fa466ef4
ducng99 <tom@tomng.dev>
2024-04-08 00:10:10 +1200

    test(ssl): Added test saving keys

--- test/ssl/key_test.go
+++ test/ssl/key_test.go
@@ -1,6 +1,8 @@
 package ssl_test
 
 import (
+	"os"
+	"path/filepath"
 	"testing"
 	"time"
 
@@ -23,5 +25,40 @@ func TestNewKeys(t *testing.T) {
 
 	if len(keyPair.Fingerprint) != 32 {
 		t.Fatalf("NewKeys() returned fingerprint with length %d, expected 32", len(keyPair.Fingerprint))
+	}
+}
+
+func TestSaveKeys(t *testing.T) {
+	keyPair, err := ssl.NewKeys(5 * time.Minute)
+	if err != nil {
+		t.Fatalf("NewKeys() returned error: %v", err)
+	}
+
+	keysSavePath, err := os.MkdirTemp(".", "goserve_*")
+	if err != nil {
+		t.Fatalf("Failed to create temporary directory: %v", err)
+	}
+
+	defer os.RemoveAll(keysSavePath)
+
+	certPath, keyPath, err := keyPair.Save(keysSavePath)
+	if err != nil {
+		t.Fatalf("Save() returned error: %v", err)
+	}
+
+	if certPath != filepath.Join(keysSavePath, ssl.CertFileName) {
+		t.Fatalf("Save() returned wrong cert path: %s", certPath)
+	}
+
+	if keyPath != filepath.Join(keysSavePath, ssl.KeyFileName) {
+		t.Fatalf("Save() returned wrong key path: %s", keyPath)
+	}
+
+	if _, err := os.Stat(certPath); err != nil {
+		t.Fatalf("Save() failed to save cert: %v", err)
+	}
+
+	if _, err := os.Stat(keyPath); err != nil {
+		t.Fatalf("Save() failed to save key: %v", err)
 	}
 }
