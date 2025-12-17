ca824e2ca7453ded3ae99a602d3abc336494174b
Thomas Nguyen <tom@tomng.dev>
2025-12-17 21:55:26 +0000

    lock tailwind cli to v3

--- internal/tmpl/dirview/themes/pretty/gen.go
+++ internal/tmpl/dirview/themes/pretty/gen.go
@@ -1,4 +1,4 @@
 package pretty
 
-//go:generate npx tailwindcss -c ./tailwind.config.js -o ./tailwind.ignore.css --minify
+//go:generate npx -y tailwindcss@3 -c ./tailwind.config.js -o ./tailwind.ignore.css --minify
 //go:generate go run ../../../../../scripts/css_to_go.go tailwind.ignore.css
